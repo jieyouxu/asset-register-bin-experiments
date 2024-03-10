5e9bcdea34386ea40d9892f9ad074dfb38224946
许杰友 Jieyou Xu (Joe) <jieyouxu@outlook.com>
2024-03-10 14:22:58 +0000

    feat: add FAssetRegistryExportPath

---
+++ src/unreal_types/fasset_registry_export_path.rs
@@ -0,0 +1,71 @@
+use std::io::{Read, Write};
+
+use byteorder::{ReadBytesExt, WriteBytesExt, LE};
+use color_eyre::eyre::{eyre, Result as EResult};
+use tracing::*;
+
+use crate::read::Readable;
+use crate::write::Writable;
+
+use super::FName;
+
+#[derive(Debug, PartialEq, Copy, Clone)]
+pub struct FAssetRegistryExportPath {
+    pub class: FName,
+    pub object: FName,
+    pub package: FName,
+}
+
+impl<W: Write> Writable<W> for FAssetRegistryExportPath {
+    #[instrument(name = "FAssetRegistryExportPath_write", skip_all)]
+    fn write(&self, writer: &mut W) -> EResult<()> {
+        self.class.write(writer)?;
+        self.object.write(writer)?;
+        self.package.write(writer)?;
+        Ok(())
+    }
+}
+
+impl<R: Read> Readable<R> for FAssetRegistryExportPath {
+    #[instrument(name = "FAssetRegistryExportPath_read", skip_all)]
+    fn read(reader: &mut R) -> EResult<Self> {
+        let class = FName::read(reader)?;
+        let object = FName::read(reader)?;
+        let package = FName::read(reader)?;
+        Ok(FAssetRegistryExportPath {
+            class,
+            object,
+            package,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::Cursor;
+
+    #[test]
+    fn test_roundtrip() {
+        let path = FAssetRegistryExportPath {
+            class: FName {
+                index: 123,
+                number: 456,
+            },
+            object: FName {
+                index: 583,
+                number: 194,
+            },
+            package: FName {
+                index: 789,
+                number: 1012,
+            },
+        };
+        let mut buf = vec![];
+        let mut writer = Cursor::new(&mut buf);
+        path.write(&mut writer).unwrap();
+        let mut reader = Cursor::new(&buf);
+        let read_path = FAssetRegistryExportPath::read(&mut reader).unwrap();
+        assert_eq!(read_path, path);
+    }
+}
--- src/unreal_types/mod.rs
+++ src/unreal_types/mod.rs
@@ -2,6 +2,8 @@
 
 mod ftext;
 mod fname;
+mod fasset_registry_export_path;
 
 pub use ftext::*;
 pub use fname::*;
+pub use fasset_registry_export_path::*;
